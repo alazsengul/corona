57967916ddf62e87383e3fa4a2792ca71f2c8eb5
Alaz Sengul <alazsengul@gmail.com>
2020-03-15 23:31:48 -0400

    Initial commit

---
+++ other/data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# CONFIRMED — https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+# DEATHS — https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+# RECOVERED — https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
+
+confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+deaths_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+confirmed = pd.read_csv(confirmed_url)
+deaths = pd.read_csv(deaths_url)
+recovered = pd.read_csv(recovered_url)
+
+print(len(confirmed), len(deaths), len(recovered))
